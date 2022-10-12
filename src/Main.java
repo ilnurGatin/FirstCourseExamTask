bce74b68aaad8d70145f2c195e90e841bdcc6e17
ilnurGatin <reilag@yandex.ru>
2022-10-12 19:48:38 +0500

    Task is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Вариант №5 Дан массив пар названий книг и авторов, необходимо привести каждую пару в строку вида:
+        // «Название книги» И.О.Автора.
+        String[][] library = {{"War and Peace", "Lev Nikolaevich Tolstoy"}, {"Reach father, poor father", "Paolo Coelho Sauza"}};
+        for (int i = 0; i < library.length; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < library[i].length; j++) {
+                if (j == 0) {
+                    stringBuilder.append('"' + library[i][j] + "\" ");
+                } else {
+                    String s = library[i][j];
+                    String[] temp = s.split(" ");
+                    for (int k = 0; k < temp.length; k++) {
+                        if (k == temp.length - 1) {
+                            stringBuilder.append(temp[k]);
+                        } else {
+                            String word = temp[k];
+                            char firstChar = word.charAt(0);
+                            stringBuilder.append(firstChar + ".");
+                        }
+                    }
+                }
+            }
+            System.out.println(stringBuilder);
+        }
     }
 }
